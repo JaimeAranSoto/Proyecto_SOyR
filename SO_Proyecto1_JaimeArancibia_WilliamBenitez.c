50f37745f197be0484eaa6838d763e4e8a293c1b
JaimeAranSoto <jaime@Jaime.localdomain>
2018-11-17 02:38:18 -0300

    Implementación de threads [Falta: Separar id de thread, de no hacerlo no realizará la suma de manera paralela]

--- SO_Proyecto1_JaimeArancibia_WilliamBenitez.c
+++ SO_Proyecto1_JaimeArancibia_WilliamBenitez.c
@@ -5,80 +5,111 @@
 
 #define dimx 100
 #define dimy 100
+#define maxValue 255
 
-typedef struct {
+typedef struct
+{
 	int x, y;
 	int mat[dimx][dimy];
-}datos;
+	int part;
+} datos;
 
-void *functioncontador1(void *i);
-void *functioncontador2(void *i);
+pthread_mutex_t contador_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condicion_var = PTHREAD_COND_INITIALIZER;
 
-int sumaMatriz(void *d, int fila1, int columna1, int fila2, int columna2){
-	datos *v = (datos *)d;
-	int suma = 0;
-	int k, j;
-	for (k = fila1; k <= fila2; k++) {
-		for (j = columna1; j <= columna2; j++) {
-			suma+=v->mat[k][j];
-		}
-	}
-	return suma;
-}
+void *functionThread(void *i);
+void imprimirMatriz(datos *d);
+void llenarMatriz(datos *v);
+int sumaMatriz(datos *d, int min, int max);
 
-
-void llenarMatriz(void *i){
-	datos *v = (datos *)i;
-	int k, j;
-	for (k = 0; k < v->x; k++) {
-		for (j = 0; j < v->y; j++) {
-			v->mat[k][j] = 1 + rand() % 255;
-		}
-	}
-}
+int suma = 0;
+int cantidadThreads =2;
 
 void main()
 {
 	int i, j;
-	pthread_t thread1, thread2;
-	
-	datos * d = (datos *)malloc(sizeof(datos));
+	pthread_t threads[cantidadThreads];
 
+	datos *d = (datos *)malloc(sizeof(datos));
+
+	//dimensiones matriz
 	d->x = 6;
 	d->y = 6;
-	llenarMatriz((void*)d);
 
-	int suma = sumaMatriz((void*)d,0,3,2,4);
-	printf("La suma de la porción de matriz es %d\n",suma);
+	llenarMatriz(d);
 
-	pthread_create(&thread1, NULL, &functioncontador1, (void *)d);
-	pthread_create(&thread2, NULL, &functioncontador2, (void *)d);
 
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
+	d->part = 0;
+	for(i=0;i<cantidadThreads;i++){
+	pthread_create(&threads[i], NULL, &functionThread, (void *)d);
+	}
+
+	for(i=0;i<cantidadThreads;i++){
+	pthread_join(threads[i], NULL);
+	}
 
 	printf("Dim x:%d\n", d->x);
 	printf("Dim y:%d\n", d->y);
 	printf("\n");
 
-	
-	for (i = 0; i < d->x; i++) {
-		for (j = 0; j < d->y; j++) {
+	int promedio = suma/(d->x*d->y);
+	printf("El promedio es %d...\n",promedio);
+
+	imprimirMatriz(d);
+
+	exit(EXIT_SUCCESS);
+}
+
+void imprimirMatriz(datos *d)
+{
+	int i, j;
+	for (i = 0; i < d->x; i++)
+	{
+		for (j = 0; j < d->y; j++)
+		{
 			printf("%d ", d->mat[i][j]);
 		}
 		printf("\n");
 	}
-	exit(EXIT_SUCCESS);
-}
-void *functioncontador1(void *i) {
-	printf("Estoy en el thread 1...\n");
-	
-	printf("Terminé thread 1...\n");
-}
-void *functioncontador2(void *d) {
-	printf("Estoy en el thread 2...\n");
-	
-	printf("Terminé el thread 2...\n");
 }
 
+void *functionThread(void *i)
+{
+	pthread_mutex_lock(&contador_mutex);
+	datos *v = (datos *)i;
+	printf("-Estoy en el thread %d...\n", v->part);
+	int suma_ = sumaMatriz(v,v->part*(v->x*v->y)/cantidadThreads,((v->part+1)*(v->x*v->y)/cantidadThreads)-1);
+	printf("Mi suma es %d\n",suma_);
+	v->part++;
+	suma+=suma_;
+	printf("Terminé thread %d\n", v->part - 1);
 
+	pthread_mutex_unlock(&contador_mutex);
+}
+
+void llenarMatriz(datos *v)
+{
+
+	int k, j;
+	for (k = 0; k < v->x; k++)
+	{
+		for (j = 0; j < v->y; j++)
+		{
+			v->mat[k][j] = 1 + rand() % maxValue;
+		}
+	}
+}
+
+int sumaMatriz(datos *d, int min, int max)
+{
+	int suma = 0;
+	int k, j;
+	
+	for (k = min; k <= max; k++)
+	{
+		int fila = k/d->y;
+		int col = k - (fila*d->y);
+		suma+=d->mat[fila][col];
+	}
+	return suma;
+}
